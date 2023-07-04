c75fedec2a56ff105eb646cc4d298a1a36d28f38
Aaroh Srivastava <aarohsjcian1@gmail.com>
2023-07-05 02:01:30 +0530

    added jwt middleware and util in SRAM

---
+++ SRAM/faculty/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from SRAM.backend.models import Faculty,FacultyCodeStatus
+from .serializers import StudentSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+
+@api_view(['POST'])
+def facultyCode(request):
+    # get data from request
+    data:Student = request.data
+    # check if data is valid
+    if data['name'] == '' or data['email'] == '' or data['roll'] == '' or data['password'] == '':
+        return Response({'message': 'Invalid Data'}, status=status.HTTP_400_BAD_REQUEST)
+    # check if roll number already exists
+    if Student.objects.filter(roll=data['roll']).exists():
+        return Response({'message': 'Roll Number already exists'}, status=status.HTTP_400_BAD_REQUEST)
+    # create new student object
+    student = Student(name=data['name'], email=data['email'], roll=data['roll'])
+    # save student object
+    student.save()
+    # return response
+    return Response({'message': 'Student Registered'}, status=status.HTTP_201_CREATED)
